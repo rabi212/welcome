8d3de96d359a044eccfde9daf736dd78b7228026
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 11:49:47 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,33 +1,46 @@
-resource "google_service_account" "github_actions" {
+# Create GitHub Actions Service Account
 
+resource "google_service_account" "github_actions" {
   account_id   = "github-actions"
   display_name = "GitHub Actions Service Account"
-
 }
 
-resource "google_project_iam_member" "artifact_writer" {
+# Owner Role (Recommended only for learning)
 
+resource "google_project_iam_member" "owner" {
   project = var.project_id
-  role    = "roles/artifactregistry.writer"
-
-  member = "serviceAccount:${google_service_account.github_actions.email}"
-
+  role    = "roles/owner"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
 
-resource "google_project_iam_member" "container_developer" {
+# Artifact Registry Administrator
 
+resource "google_project_iam_member" "artifact_registry" {
   project = var.project_id
-  role    = "roles/container.developer"
-
-  member = "serviceAccount:${google_service_account.github_actions.email}"
-
+  role    = "roles/artifactregistry.admin"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
+
+# Kubernetes Engine Admin
+
+resource "google_project_iam_member" "gke_admin" {
+  project = var.project_id
+  role    = "roles/container.admin"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+# Service Account User
+
+resource "google_project_iam_member" "service_account_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
+}
+
+# Storage Admin
 
 resource "google_project_iam_member" "storage_admin" {
-
   project = var.project_id
   role    = "roles/storage.admin"
-
-  member = "serviceAccount:${google_service_account.github_actions.email}"
-
+  member  = "serviceAccount:${google_service_account.github_actions.email}"
 }
