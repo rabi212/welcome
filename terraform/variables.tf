f0f5c22057bc73bab41115a764a42d3017111965
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 10:57:09 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,27 @@
+variable "project_id" {
+  default = "billmonitor"
+}
+
+variable "region" {
+  default = "asia-south1"
+}
+
+variable "zone" {
+  default = "asia-south1-a"
+}
+
+variable "cluster_name" {
+  default = "welcome-cluster"
+}
+
+variable "repository_name" {
+  default = "docker-repo"
+}
+
+variable "machine_type" {
+  default = "e2-medium"
+}
+
+variable "node_count" {
+  default = 1
+}
