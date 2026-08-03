ac9cb7a68fde06ab2477ab722bb471770c55d6e4
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 11:45:10 +0530

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,17 +1,10 @@
 terraform {
-
   required_version = ">= 1.6"
 
   required_providers {
-
     google = {
-
-      source = "hashicorp/google"
-
+      source  = "hashicorp/google"
       version = "~> 6.0"
-
     }
-
   }
-
 }
