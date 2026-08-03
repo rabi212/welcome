0ebd05144cd079328a1c0a983ea898d14f505104
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 10:58:20 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,1 +1,13 @@
-resource "google_container_cluster"
+resource "google_container_cluster" "primary" {
+
+  name     = var.cluster_name
+
+  location = var.zone
+
+  deletion_protection = false
+
+  remove_default_node_pool = true
+
+  initial_node_count = 1
+
+}
