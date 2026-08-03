da89f11c945bfd872078eeecfa3092052b338fa7
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 11:01:28 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,3 +1,33 @@
-google_service_account
+resource "google_service_account" "github_actions" {
 
-google_project_iam_member
+  account_id   = "github-actions"
+  display_name = "GitHub Actions Service Account"
+
+}
+
+resource "google_project_iam_member" "artifact_writer" {
+
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+
+  member = "serviceAccount:${google_service_account.github_actions.email}"
+
+}
+
+resource "google_project_iam_member" "container_developer" {
+
+  project = var.project_id
+  role    = "roles/container.developer"
+
+  member = "serviceAccount:${google_service_account.github_actions.email}"
+
+}
+
+resource "google_project_iam_member" "storage_admin" {
+
+  project = var.project_id
+  role    = "roles/storage.admin"
+
+  member = "serviceAccount:${google_service_account.github_actions.email}"
+
+}
