cc58638ef4f2c59f3b98200cf926bd21a0a4513c
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 12:21:04 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,27 +1,8 @@
-locals {
-  github_service_account = "github-actions@billmonitor.iam.gserviceaccount.com"
-}
+# IAM is managed manually.
 
-resource "google_project_iam_member" "artifact_registry_admin" {
-  project = var.project_id
-  role    = "roles/artifactregistry.admin"
-  member  = "serviceAccount:${local.github_service_account}"
-}
+# Service Account:
+# github-actions@billmonitor.iam.gserviceaccount.com
 
-resource "google_project_iam_member" "container_admin" {
-  project = var.project_id
-  role    = "roles/container.admin"
-  member  = "serviceAccount:${local.github_service_account}"
-}
+# Required roles have already been assigned manually in GCP.
 
-resource "google_project_iam_member" "storage_admin" {
-  project = var.project_id
-  role    = "roles/storage.admin"
-  member  = "serviceAccount:${local.github_service_account}"
-}
-
-resource "google_project_iam_member" "service_account_user" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${local.github_service_account}"
-}
+# No Terraform IAM resources are required.
