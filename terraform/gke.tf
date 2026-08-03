bdffa8c960741a4d63e75b4f8f5a409148760b76
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 11:00:48 +0530

    Update gke.tf

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -1,13 +1,40 @@
 resource "google_container_cluster" "primary" {
 
   name     = var.cluster_name
-
   location = var.zone
 
-  deletion_protection = false
-
+  deletion_protection      = false
   remove_default_node_pool = true
+  initial_node_count       = 1
 
-  initial_node_count = 1
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+
+  name     = "primary-node-pool"
+  cluster  = google_container_cluster.primary.name
+  location = var.zone
+
+  node_count = var.node_count
+
+  node_config {
+
+    machine_type = var.machine_type
+
+    spot = true
+
+    disk_size_gb = 20
+
+    disk_type = "pd-balanced"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+
+    labels = {
+      environment = "dev"
+    }
+
+  }
 
 }
