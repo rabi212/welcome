4596cedb336601653f33090d13053481fa0480c0
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 11:46:48 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,7 @@
-output "project_id" {
-  value = var.project_id
-}
-
 output "cluster_name" {
   value = google_container_cluster.primary.name
 }
 
-output "artifact_registry" {
+output "repository" {
   value = google_artifact_registry_repository.docker_repo.repository_id
-}
-
-output "service_account" {
-  value = google_service_account.github_actions.email
 }
