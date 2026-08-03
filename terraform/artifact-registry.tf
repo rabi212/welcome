ebc1d9a6a7da1fb6122bae7ecdad3dc3e6289d5c
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 10:57:50 +0530

    Update artifact-registry.tf

--- terraform/artifact-registry.tf
+++ terraform/artifact-registry.tf
@@ -1,1 +1,11 @@
-resource "google_artifact_registry_repository"
+resource "google_artifact_registry_repository" "docker_repo" {
+
+  location      = var.region
+
+  repository_id = var.repository_name
+
+  description   = "Docker Repository"
+
+  format        = "DOCKER"
+
+}
