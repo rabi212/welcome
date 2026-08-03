a23dcb83726524b65d1fe3005a77ed09a215c2f8
rabi212 <144771474+rabi212@users.noreply.github.com>
2026-08-03 11:52:11 +0530

    Update iam.tf

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,46 +1,27 @@
-# Create GitHub Actions Service Account
-
-resource "google_service_account" "github_actions" {
-  account_id   = "github-actions"
-  display_name = "GitHub Actions Service Account"
+locals {
+  github_service_account = "github-actions@billmonitor.iam.gserviceaccount.com"
 }
 
-# Owner Role (Recommended only for learning)
-
-resource "google_project_iam_member" "owner" {
-  project = var.project_id
-  role    = "roles/owner"
-  member  = "serviceAccount:${google_service_account.github_actions.email}"
-}
-
-# Artifact Registry Administrator
-
-resource "google_project_iam_member" "artifact_registry" {
+resource "google_project_iam_member" "artifact_registry_admin" {
   project = var.project_id
   role    = "roles/artifactregistry.admin"
-  member  = "serviceAccount:${google_service_account.github_actions.email}"
+  member  = "serviceAccount:${local.github_service_account}"
 }
 
-# Kubernetes Engine Admin
-
-resource "google_project_iam_member" "gke_admin" {
+resource "google_project_iam_member" "container_admin" {
   project = var.project_id
   role    = "roles/container.admin"
-  member  = "serviceAccount:${google_service_account.github_actions.email}"
+  member  = "serviceAccount:${local.github_service_account}"
 }
-
-# Service Account User
-
-resource "google_project_iam_member" "service_account_user" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.github_actions.email}"
-}
-
-# Storage Admin
 
 resource "google_project_iam_member" "storage_admin" {
   project = var.project_id
   role    = "roles/storage.admin"
-  member  = "serviceAccount:${google_service_account.github_actions.email}"
+  member  = "serviceAccount:${local.github_service_account}"
+}
+
+resource "google_project_iam_member" "service_account_user" {
+  project = var.project_id
+  role    = "roles/iam.serviceAccountUser"
+  member  = "serviceAccount:${local.github_service_account}"
 }
